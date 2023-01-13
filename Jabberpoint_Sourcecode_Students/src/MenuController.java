7a22fd129b192e372a72a3aeb178bddacac6d2a0
EvaldN <91316779+EvaldN@users.noreply.github.com>
2023-01-13 20:55:16 +0100

    Assignment
    pushed the assignment

---
+++ Jabberpoint_Sourcecode_Students/src/MenuController.java
@@ -0,0 +1,111 @@
+import java.awt.MenuBar;
+import java.awt.Frame;
+import java.awt.Menu;
+import java.awt.MenuItem;
+import java.awt.MenuShortcut;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+
+import javax.swing.JOptionPane;
+
+/** <p>The controller for the menu</p>
+ * @author Ian F. Darwin, ian@darwinsys.com, Gert Florijn, Sylvia Stuurman
+ * @version 1.1 2002/12/17 Gert Florijn
+ * @version 1.2 2003/11/19 Sylvia Stuurman
+ * @version 1.3 2004/08/17 Sylvia Stuurman
+ * @version 1.4 2007/07/16 Sylvia Stuurman
+ * @version 1.5 2010/03/03 Sylvia Stuurman
+ * @version 1.6 2014/05/16 Sylvia Stuurman
+ */
+public class MenuController extends MenuBar {
+
+	private Frame parent; //The frame, only used as parent for the Dialogs
+
+	protected static final String IOEX = "IO Exception: ";
+	protected static final String LOADERR = "Load Error";
+	protected static final String SAVEERR = "Save Error";
+
+	public MenuController(Frame frame, SlideViewerComponent slideViewerComponent) {
+		parent = frame;
+		MenuItem menuItem;
+		Menu fileMenu = new Menu("File");
+		fileMenu.add(menuItem = mkMenuItem("Open"));
+		menuItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent actionEvent) {
+				slideViewerComponent.clear();
+				Accessor xmlAccessor = new XMLAccessor();
+				try {
+					xmlAccessor.loadFile(slideViewerComponent.getPresentation(), "testPresentation.xml");
+					slideViewerComponent.setSlideNumber(0);
+				} catch (IOException exc) {
+					JOptionPane.showMessageDialog(parent, IOEX + exc,
+         			LOADERR, JOptionPane.ERROR_MESSAGE);
+				}
+				parent.repaint();
+			}
+		} );
+		fileMenu.add(menuItem = mkMenuItem("New"));
+		menuItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent actionEvent) {
+				slideViewerComponent.clear();
+				parent.repaint();
+			}
+		});
+		fileMenu.add(menuItem = mkMenuItem("Save"));
+		menuItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Accessor xmlAccessor = new XMLAccessor();
+				try {
+					xmlAccessor.saveFile(slideViewerComponent.getPresentation(), "savedPresentation.xml");
+				} catch (IOException exc) {
+					JOptionPane.showMessageDialog(parent, IOEX + exc,
+							SAVEERR, JOptionPane.ERROR_MESSAGE);
+				}
+			}
+		});
+		fileMenu.addSeparator();
+		fileMenu.add(menuItem = mkMenuItem("Exit"));
+		menuItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent actionEvent) {
+				slideViewerComponent.getPresentation().exit(0);
+			}
+		});
+		add(fileMenu);
+		Menu viewMenu = new Menu("View");
+		viewMenu.add(menuItem = mkMenuItem("New"));
+		menuItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent actionEvent) {
+				slideViewerComponent.nextSlide();
+			}
+		});
+		viewMenu.add(menuItem = mkMenuItem("Prev"));
+		menuItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent actionEvent) {
+				slideViewerComponent.prevSlide();
+			}
+		});
+		viewMenu.add(menuItem = mkMenuItem("Go to"));
+		menuItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent actionEvent) {
+				String pageNumberStr = JOptionPane.showInputDialog((Object)"Page number?");
+				int pageNumber = Integer.parseInt(pageNumberStr);
+				slideViewerComponent.setSlideNumber(pageNumber - 1);
+			}
+		});
+		add(viewMenu);
+		Menu helpMenu = new Menu("Help");
+		helpMenu.add(menuItem = mkMenuItem("About"));
+		menuItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent actionEvent) {
+				AboutBox.show(parent);
+			}
+		});
+		setHelpMenu(helpMenu);		//Needed for portability (Motif, etc.).
+	}
+
+//Creating a menu-item
+	public MenuItem mkMenuItem(String name) {
+		return new MenuItem(name, new MenuShortcut(name.charAt(0)));
+	}
+}
